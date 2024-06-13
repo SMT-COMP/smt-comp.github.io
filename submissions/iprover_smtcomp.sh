f6770f6650d9db4853a69d13e02c44bd6495ddea
Konstantin Korovin <kostyakor@gmail.com>
2024-06-13 15:56:29 +0100

    pp json

--- submissions/iprover_smtcomp.sh
+++ submissions/iprover_smtcomp.sh
@@ -1,17 +1,54 @@
-{    "name": "iProver v3.9",
-    "contributors": [{ "name": "Konstantin Korovin", "website": "http://www.cs.man.ac.uk/~korovink/" }],
-    "contacts": ["Konstantin Korovin <kostyakor@gmail.com>"],
-    "archive": {
-        "url": "https://zenodo.org/records/11636244",
-        "h": { "sha256": "8f7f4bad13e87d6aaa1bb8ca78b102edaa5495caf65d5fe39c3c16d6f70003d6" }},
-    "website": "https://gitlab.com/korovin/iprover",
-    "system_description": "http://www.cs.man.ac.uk/~korovink/iprover-smt-comp-2024.pdf",
-    "command": ["cd bin; iprover_smtcomp.sh"],    
-    "solver_type": "wrapped",
-    "participations": [
-        { "tracks": ["SingleQuery"],
-          "divisions" :["Arith","Equality","Equality+LinearArith","Equality+NonLinearArith"],        
-          "logics": ["ALIA, ANIA, AUFDTLIA, AUFDTLIRA, AUFDTNIRA, AUFLIA, AUFLIRA, AUFNIA, AUFNIRA, LIA, LRA, NIA, NRA, UF, UFDT, UFDTLIA, UFDTLIRA, UFDTNIA, UFDTNIRA, UFIDL, UFLIA, UFLRA, UFNIA, UFNIRA"] },
-        { "tracks": ["Parallel"],
-          "divisions" :["Arith","Equality","Equality+LinearArith","Equality+NonLinearArith"],          
-            "logics": ["ALIA, ANIA, AUFDTLIA, AUFDTLIRA, AUFDTNIRA, AUFLIA, AUFLIRA, AUFNIA, AUFNIRA, LIA, LRA, NIA, NRA, UF, UFDT, UFDTLIA, UFDTLIRA, UFDTNIA, UFDTNIRA, UFIDL, UFLIA, UFLRA, UFNIA, UFNIRA"]}]}
+{
+   "archive" : {
+      "h" : {
+         "sha256" : "8f7f4bad13e87d6aaa1bb8ca78b102edaa5495caf65d5fe39c3c16d6f70003d6"
+      },
+      "url" : "https://zenodo.org/records/11636244"
+   },
+   "command" : [
+      "cd bin; iprover_smtcomp.sh"
+   ],
+   "contacts" : [
+      "Konstantin Korovin <kostyakor@gmail.com>"
+   ],
+   "contributors" : [
+      {
+         "name" : "Konstantin Korovin",
+         "website" : "http://www.cs.man.ac.uk/~korovink/"
+      }
+   ],
+   "name" : "iProver v3.9",
+   "participations" : [
+      {
+         "divisions" : [
+            "Arith",
+            "Equality",
+            "Equality+LinearArith",
+            "Equality+NonLinearArith"
+         ],
+         "logics" : [
+            "ALIA, ANIA, AUFDTLIA, AUFDTLIRA, AUFDTNIRA, AUFLIA, AUFLIRA, AUFNIA, AUFNIRA, LIA, LRA, NIA, NRA, UF, UFDT, UFDTLIA, UFDTLIRA, UFDTNIA, UFDTNIRA, UFIDL, UFLIA, UFLRA, UFNIA, UFNIRA"
+         ],
+         "tracks" : [
+            "SingleQuery"
+         ]
+      },
+      {
+         "divisions" : [
+            "Arith",
+            "Equality",
+            "Equality+LinearArith",
+            "Equality+NonLinearArith"
+         ],
+         "logics" : [
+            "ALIA, ANIA, AUFDTLIA, AUFDTLIRA, AUFDTNIRA, AUFLIA, AUFLIRA, AUFNIA, AUFNIRA, LIA, LRA, NIA, NRA, UF, UFDT, UFDTLIA, UFDTLIRA, UFDTNIA, UFDTNIRA, UFIDL, UFLIA, UFLRA, UFNIA, UFNIRA"
+         ],
+         "tracks" : [
+            "Parallel"
+         ]
+      }
+   ],
+   "solver_type" : "wrapped",
+   "system_description" : "http://www.cs.man.ac.uk/~korovink/iprover-smt-comp-2024.pdf",
+   "website" : "https://gitlab.com/korovin/iprover"
+}
--- submissions/template/generate_Readme.py
+++ submissions/template/generate_Readme.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from string import Template
 from urllib.parse import quote
 
-#import rich
+import rich
 import typer
 
 app = typer.Typer()
@@ -35,12 +35,10 @@ def check():
     current = dst.read_text()
     oracle = substitute()
     if current == oracle:
-#        rich.print(":white_check_mark: submissions/Readme.md is up to date")
+        rich.print(":white_check_mark: submissions/Readme.md is up to date")
         exit(0)
     else:
-#        rich.print(":heavy_exclamation_mark: submissions/Readme.md is obsolete!")
- #
-    
+        rich.print(":heavy_exclamation_mark: submissions/Readme.md is obsolete!")
         exit(1)
 
 
