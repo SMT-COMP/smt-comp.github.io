b6502730323233839d1aeda5cbed6910cce6a37e
Martin Jonáš <martin.jonas@mail.muni.cz>
2024-06-20 09:35:16 +0200

    Add script for enforcing wall time on VerifierCloud.

--- smtcomp/walltime_killer.py
+++ smtcomp/walltime_killer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import sys
-
 sys.path.insert(0, "benchexec")
 
 from benchexec.runexecutor import RunExecutor
@@ -9,16 +8,17 @@ import logging
 
 logging.basicConfig(level=logging.ERROR)
 
-
 def main() -> None:
     if len(sys.argv) < 3:
         print("Usage: ./walltime_killer.py WALLTIME COMMAND")
-
     outfile = "runexec_output.log"
 
     executor = RunExecutor(use_namespaces=False)
     result = executor.execute_run(
-        args=sys.argv[2:], walltimelimit=int(sys.argv[1]), output_filename=outfile, write_header=False
+        args=sys.argv[2:],
+        walltimelimit=int(sys.argv[1]),
+        output_filename=outfile,
+        write_header=False
     )
 
     try:
@@ -27,12 +27,11 @@ def main() -> None:
                 print(line)
     except Exception as e:
         pass
-
-    if result.get("terminationreason") == "walltime":
+    if result.get('terminationreason') == "walltime":
         print("TIMEOUT")
         exit(9)
 
-    exit(result["exitcode"].raw)
+    exit(result['exitcode'].raw)
 
 
 if __name__ == "__main__":
