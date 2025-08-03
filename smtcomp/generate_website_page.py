5aef59f3fa62719e6877ce8e91a90d0c94fec306
Martin Jonáš <martin.jonas@mail.muni.cz>
2025-08-03 21:11:50 +0200

    feat: Add BestOverall ranking to certificates.

--- smtcomp/generate_website_page.py
+++ smtcomp/generate_website_page.py
@@ -230,7 +230,7 @@ class PodiumLargestContribution(BaseModel):
 
 
 class PodiumCrossDivision(RootModel):
-    root: PodiumLargestContribution | PodiumBiggestLead = Field(..., discriminator="recognition")
+    root: PodiumBestOverall | PodiumLargestContribution | PodiumBiggestLead = Field(..., discriminator="recognition")
 
 
 class Podium(RootModel):
