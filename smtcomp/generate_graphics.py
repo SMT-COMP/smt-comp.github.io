5afdde9da03c1cf0f27bdd02ed95b7534bc83011
François Bobot <francois.bobot@cea.fr>
2025-08-07 22:15:53 +0200

    [Graph] selection by answers

--- smtcomp/generate_graphics.py
+++ smtcomp/generate_graphics.py
@@ -16,6 +16,7 @@ c_logic = pl.col("logic")
 c_solver = pl.col("solver")
 c_solver2 = pl.col("solver2")
 c_answer = pl.col("answer")
+c_answer2 = pl.col("answer2")
 c_cputime_s = pl.col("cputime_s")
 c_cputime_s2 = pl.col("cputime_s2")
 c_run = pl.col("run")
@@ -24,6 +25,8 @@ c_bucket2 = pl.col("bucket2")
 
 
 def output_for_logic(config: defs.Config, results: pl.LazyFrame, logic: defs.Logic, output_dir: Path) -> None:
+
+    # We are computing the buckets offline because we have too much data
     results = results.filter(c_run == True, c_logic == int(logic)).select(
         c_file,
         c_solver,
@@ -32,7 +35,8 @@ def output_for_logic(config: defs.Config, results: pl.LazyFrame, logic: defs.Log
         bucket=pl.lit(10.0).pow(c_cputime_s.log(10).floor()),
         # bucket=c_cputime_s.log(10).floor(),
     )
-    results_with = results.select(c_file, solver2=c_solver, bucket2=c_bucket, cputime_s2=c_cputime_s)
+
+    results_with = results.select(c_file, solver2=c_solver, bucket2=c_bucket, cputime_s2=c_cputime_s, answer2=c_answer)
     results = results.join(results_with, on=c_file, how="left")
 
     corr = (
@@ -41,22 +45,38 @@ def output_for_logic(config: defs.Config, results: pl.LazyFrame, logic: defs.Log
         .sort(c_solver, c_solver2)
     )
 
-    results = results.group_by(c_solver, c_solver2, c_bucket, c_bucket2).len().sort(c_solver, c_solver2)
+    answers = pl.DataFrame(
+        data=((int(n), str(n)) for n in defs.Answer), schema=[("answer", pl.Int8), ("pretty_answer", pl.String)]
+    ).lazy()
 
-    df_corr, df_results = pl.collect_all([corr, results])
+    def pretty_answer(lf: pl.LazyFrame, replaced: str) -> pl.LazyFrame:
+        if replaced == "answer":
+            pretty = answers
+        else:
+            pretty = answers.rename({"answer": replaced})
+        return lf.join(pretty, how="left", on=replaced).drop(replaced).rename({"pretty_answer": replaced})
+
+    results = (
+        results.group_by(c_solver, c_solver2, c_answer, c_answer2, c_bucket, c_bucket2).len().sort(c_solver, c_solver2)
+    )
+
+    results = pretty_answer(pretty_answer(results, "answer"), "answer2")
+
+    buckets = results.select(c_bucket.unique())
+
+    df_corr, df_results, df_buckets = pl.collect_all([corr, results, buckets])
+
+    bucket_domain = list(df_buckets["bucket"])
 
     row1 = df_corr.row(1, named=True)
 
     # Create heatmap with selection
     select_x = alt.selection_point(fields=["solver"], name="solver1", value=row1["solver"], toggle=False)
     select_y = alt.selection_point(fields=["solver2"], name="solver2", value=row1["solver2"], toggle=False)
+    answer_x = alt.selection_point(fields=["answer"], name="answer1")
+    answer_y = alt.selection_point(fields=["answer2"], name="answer2")
     g_select_provers = (
-        alt.Chart(
-            df_corr,
-            title="Click a tile to compare solvers",
-            height=250,
-            width=250,
-        )
+        alt.Chart(df_corr, title="Click a tile to compare solvers", height=250, width=250)
         .mark_rect()
         .encode(
             alt.X("solver", title=None),
@@ -70,14 +90,16 @@ def output_for_logic(config: defs.Config, results: pl.LazyFrame, logic: defs.Log
     title = alt.Title(alt.expr(f'{select_x.name}.solver + " vs " + {select_y.name}.solver2'))
     g_results = (
         alt.Chart(df_results, title=title, height=250, width=250)
-        .transform_filter(select_x, select_y)
+        .transform_filter(select_x, select_y, answer_x, answer_y)
         .transform_aggregate(benchs="sum(len)", groupby=["bucket", "bucket2"])
-        .add_params(select_x, select_y)
+        .add_params(select_x, select_y, answer_x, answer_y)
         .encode(
             alt.X("bucket:O").axis(title="solver1", bandPosition=0.5)
             # align should move the center of the cell, it does not
-            .scale(type="band", align=0),
-            alt.Y("bucket2:O").axis(title="solver2", bandPosition=0.5).scale(type="band", align=0).sort("descending"),
+            .scale(type="band", align=0, domain=bucket_domain),
+            alt.Y("bucket2:O")
+            .axis(title="solver2", bandPosition=0.5)
+            .scale(type="band", align=0, domain=reversed(bucket_domain)),
             text="benchs:Q",
         )
     )
@@ -86,7 +108,41 @@ def output_for_logic(config: defs.Config, results: pl.LazyFrame, logic: defs.Log
 
     g_results_text = g_results.mark_text(baseline="middle")
 
+    # g_answers = (
+    #     alt.Chart(df_results, title="Answers")
+    #     .transform_filter(select_x, select_y, answer_x, answer_y)
+    #     .transform_aggregate(benchs="sum(len)", groupby=["answer_x", "bucket2"])
+    #     .add_params(select_x, select_y, answer_x, answer_y)
+    #     .encode(
+    #         alt.X("bucket:O").axis(title="solver1", bandPosition=0.5)
+    #         # align should move the center of the cell, it does not
+    #         .scale(type="band", align=0),
+    #         alt.Y("bucket2:O").axis(title="solver2", bandPosition=0.5).scale(type="band", align=0).sort("descending"),
+    #         text="benchs:Q",
+    #     )
+    #     .mark_bar()
+    # )
+    opacity = alt.when(select_x, select_y, answer_x, answer_y).then(alt.value(1)).otherwise(alt.value(0.0))
+
+    legend_answer_x = (
+        alt.Chart(df_results, title=alt.Title(alt.expr(f'"solver1:" + {select_x.name}.solver')))
+        .mark_point()
+        .encode(alt.Y("answer:N").axis(title="", orient="right"), opacity=opacity)
+        .add_params(answer_x)
+    )
+
+    legend_answer_y = (
+        alt.Chart(df_results, title=alt.Title(alt.expr(f'"solver2:" + {select_y.name}.solver2')))
+        .mark_point()
+        .encode(alt.Y("answer2:N").axis(title="", orient="right"), opacity=opacity)
+        .add_params(answer_y)
+    )
+
     graph = (g_select_provers | g_results_rect + g_results_text).resolve_scale(color="independent")
+
+    graph = alt.vconcat(graph, legend_answer_x | legend_answer_y)
+
+    graph = graph.resolve_scale(color="independent")
 
     graph.save(output_dir / "test.html")
     graph.save(output_dir / "test.json", format="json")
