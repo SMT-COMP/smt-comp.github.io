431f7acc4af6cbef6b23f6d274c2764319cec61c
Martin Jonáš <martin.jonas@mail.muni.cz>
2026-06-25 10:01:36 +0200

    fix: Shorten names of generated YML benchmark files.
    Avoids the issue when BenchCloud silently drops results with too long file
    names (result of solver name + benchmark name).

--- smtcomp/scramble_benchmarks.py
+++ smtcomp/scramble_benchmarks.py
@@ -85,9 +85,13 @@ def scramble_file(fdict: dict, incremental: bool, srcdir: Path, dstdir: Path, ar
     expected = get_expected_result(orig_path) if not incremental else None
 
     mangled_name = "_".join(
-        [str(fdict["file"]), str(defs.Logic.of_int(fdict["logic"])), fdict["family"].replace("/", "__"), fdict["name"]]
+        [str(fdict["file"]),
+         str(defs.Logic.of_int(fdict["logic"])),
+         fdict["family"].
+         replace("/", "__"),
+         fdict["name"]]
     )
-    yaml_dst = dstdir.joinpath(mangled_name).with_suffix(".yml")
+    yaml_dst = dstdir.joinpath(mangled_name[:150]).with_suffix(".yml")
 
     generate_benchmark_yml(yaml_dst, scrambled_path, expected, orig_path.relative_to(srcdir))
 
